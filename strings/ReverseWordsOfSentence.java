7dfa54205e17977b050b171e2ad630742f6b2085
Pavithra_Reddy <pavithram28@gmail.com>
2021-07-12 20:55:26 +0530

    Reverse Words of String

---
+++ strings/ReverseWordsOfSentence.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*	
+ 	Given an input string, reverse the string word by word.
+	Input : "the sky is blue",
+	Output : "blue is sky the".
+ */
+
+
+class ReverseWordsOfSentence {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(reverseWords(s));
+		
+		sc.close();
+	}
+	
+	public static String reverseWords(String s) {
+		String words[] = s.split("\\s+");
+		String res = "";
+		for(int i = words.length - 1; i > 0 ; i--) {
+			res += words[i] + " ";
+		}
+		res += words[0];
+		return res;
+    }	
+}
