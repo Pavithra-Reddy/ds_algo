9b93930745f37482e58fa8fcb34648e180218fc6
Pavithra Reddy <pavithram28@gmail.com>
2021-08-29 18:46:48 +0530

    Patch Array

---
+++ java/leetcode_aug/PatchArray.java
@@ -0,0 +1,29 @@
+package leetcode_aug; 
+
+class PatchArray{
+
+public static void main(String args[]){
+
+int nums = {1,4,10};
+int n= 50;
+
+System.out.println(minPatches(nums,n));
+
+}
+public static int minPatches(int[] nums, int n) {
+long miss = 1;
+int count = 0;
+int i = 0;
+while(miss <= n){
+if(i<nums.length && nums[i] <= miss){
+miss += nums[i];
+i++;
+}else{
+miss += miss;
+count++;
+}
+}
+return count; 
+        
+    }
+}
